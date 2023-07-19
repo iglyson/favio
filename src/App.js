74d118176ca82a26729a7b650550a4ae602de173
iglyson <118455831+iglyson@users.noreply.github.com>
2023-07-19 11:17:09 -0300

    primeira versao

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './banco-do-brasil.png';
+import Cabecalho from './componentes/Cabecalho';
+import Conteudo,{OlaMundo} from './componentes/Conteudo';
+import Rodape from './componentes/Rodape';
+/*
+arquivo app jsx
+componente funcional = porque ele é uma funcao
+*/ 
 function App() {
+
+  OlaMundo();
+  /*todo componente deve retornar um html entre parenteses*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /*o react substitui classname por class pq class é palavra reservada do JS*/
+    <>
+    <Cabecalho logo="logo192.png"titulo="Favio.io" subtitulo="o melhor gerenciador de favoritos da internet brasileira"/>
+    <Conteudo/>
+    <Rodape/>
+
+    </>
   );
 }
 
---
+++ src/componentes/Conteudo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Conteudo() {
+  return (
+    <div>Conteudo</div>
+
+
+  )
+}
+
+export function OlaMundo(){
+    console.log("ola mundo")
+}
